957695f50cf1f61a296f8588616819cae1bbe1ed
Kabirdas Henry <kbrdsk@gmail.com>
2020-05-04 18:02:37 -0700

    more tab experimentation, split stylesheet

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './styles/style.css';
+import './styles/navbar.css';
+import './styles/content.css';
+
 import {Tab} from './tab.js';
 
 let body = document.querySelector('body');
@@ -9,10 +12,24 @@ navContainer.classList.add('nav-container');
 let contentContainer = document.createElement('div');
 contentContainer.classList.add('content-container');
 
-for(let i = 0; i < 3; i++){
-	navContainer.appendChild((new Tab()).nav);
+let activeTab;
+
+function activateTab(tab){
+	if(activeTab) activeTab.active = false;
+	tab.active = true;
+	activeTab = tab;
 }
 
+let tabs = [];
+for(let i = 0; i < 3; i++){
+	let tab = new Tab();
+	tabs.push(tab);
+
+	tab.nav.addEventListener('click', e => activateTab(e.target.tab));
+
+	navContainer.appendChild(tab.nav);
+	contentContainer.appendChild(tab.content);
+}
 
 body.appendChild(navContainer);
 body.appendChild(contentContainer);
--- src/tab.js
+++ src/tab.js
@@ -1,20 +1,34 @@
 export class Tab {
 	constructor(title = 'New Tab'){
+		this._active = false;
+
 		this._navIcon = new Image();
+		this._navIcon.classList.add('nav-icon');
 
 		this._navTitle = document.createElement('p');
+		this._navTitle.classList.add('nav-title');
 		this.title = title;
 
 		this._nav = document.createElement('div');
 		this._nav.classList.add('tab-nav');
 		this._nav.appendChild(this._navIcon);
 		this._nav.appendChild(this._navTitle);
+		this._nav.tab = this;
 
 		this._content = document.createElement('div');
+		this._content.classList.add('tab-content');
+
+		this.active = false;
 	}
 
 	set navIcon(iconSrc){
 		this._navIcon.src = iconSrc; 
+	}
+
+	set active(isActive){
+		this._nav.setAttribute('active', isActive);
+		this._content.setAttribute('active', isActive);
+		this._active = isActive;
 	}
 
 	get navIcon(){
