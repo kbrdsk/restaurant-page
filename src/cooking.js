da3bcef73f5d8c5cb3878f5dd7559a216e8def08
Kabirdas Henry <kbrdsk@gmail.com>
2020-05-07 12:06:44 -0700

    added filler content to cooking tab

--- src/cooking.js
+++ src/cooking.js
@@ -1,3 +1,20 @@
 let cookingContent = document.createElement('div');
 
 export default cookingContent;
+
+let title = document.createElement('h3');
+title.textContent = 'Some of our favorite recipes!';
+
+let recipeListDisplay = document.createElement('ul');
+
+let recipeList = [];
+
+for(let i = 0; i < 4; i++){
+	let recipe = document.createElement('li');
+	recipe.textContent = 'Some amazing recipe';
+	recipeList.push(recipe);
+	recipeListDisplay.appendChild(recipe);
+}
+
+cookingContent.appendChild(title);
+cookingContent.appendChild(recipeListDisplay);
